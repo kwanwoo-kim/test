e41d4b7576055f2f3155ec9a291fcee589ad6a7b
kwanwoo.kim@gmail.com <kwanwoo.kim@gmail.com>
2019-01-05 10:03:09 +0900

    modify

--- layers.py
+++ layers.py
@@ -17,6 +17,7 @@ class Relu:
 
     def backward(self, dout):
         dout[self.mask] = 0
+        #dx = dout
         dx = dout
 
         return dx
@@ -41,7 +42,7 @@ class Affine:
     def __init__(self, W, b):
         self.W = W
         self.b = b
-        
+
         self.x = None
         self.original_x_shape = None
         # 가중치와 편향 매개변수의 미분
@@ -62,7 +63,7 @@ class Affine:
         dx = np.dot(dout, self.W.T)
         self.dW = np.dot(self.x.T, dout)
         self.db = np.sum(dout, axis=0)
-        
+
         dx = dx.reshape(*self.original_x_shape)  # 입력 데이터 모양 변경(텐서 대응)
         return dx
 
@@ -72,12 +73,12 @@ class SoftmaxWithLoss:
         self.loss = None # 손실함수
         self.y = None    # softmax의 출력
         self.t = None    # 정답 레이블(원-핫 인코딩 형태)
-        
+
     def forward(self, x, t):
         self.t = t
         self.y = softmax(x)
         self.loss = cross_entropy_error(self.y, self.t)
-        
+
         return self.loss
 
     def backward(self, dout=1):
@@ -88,7 +89,7 @@ class SoftmaxWithLoss:
             dx = self.y.copy()
             dx[np.arange(batch_size), self.t] -= 1
             dx = dx / batch_size
-        
+
         return dx
 
 
@@ -119,12 +120,12 @@ class BatchNormalization:
         self.gamma = gamma
         self.beta = beta
         self.momentum = momentum
-        self.input_shape = None # 합성곱 계층은 4차원, 완전연결 계층은 2차원  
+        self.input_shape = None # 합성곱 계층은 4차원, 완전연결 계층은 2차원
 
         # 시험할 때 사용할 평균과 분산
         self.running_mean = running_mean
-        self.running_var = running_var  
-        
+        self.running_var = running_var
+
         # backward 시에 사용할 중간 데이터
         self.batch_size = None
         self.xc = None
@@ -139,33 +140,33 @@ class BatchNormalization:
             x = x.reshape(N, -1)
 
         out = self.__forward(x, train_flg)
-        
+
         return out.reshape(*self.input_shape)
-            
+
     def __forward(self, x, train_flg):
         if self.running_mean is None:
             N, D = x.shape
             self.running_mean = np.zeros(D)
             self.running_var = np.zeros(D)
-                        
+
         if train_flg:
             mu = x.mean(axis=0)
             xc = x - mu
             var = np.mean(xc**2, axis=0)
             std = np.sqrt(var + 10e-7)
             xn = xc / std
-            
+
             self.batch_size = x.shape[0]
             self.xc = xc
             self.xn = xn
             self.std = std
             self.running_mean = self.momentum * self.running_mean + (1-self.momentum) * mu
-            self.running_var = self.momentum * self.running_var + (1-self.momentum) * var            
+            self.running_var = self.momentum * self.running_var + (1-self.momentum) * var
         else:
             xc = x - self.running_mean
             xn = xc / ((np.sqrt(self.running_var + 10e-7)))
-            
-        out = self.gamma * xn + self.beta 
+
+        out = self.gamma * xn + self.beta
         return out
 
     def backward(self, dout):
@@ -188,10 +189,10 @@ class BatchNormalization:
         dxc += (2.0 / self.batch_size) * self.xc * dvar
         dmu = np.sum(dxc, axis=0)
         dx = dxc - dmu / self.batch_size
-        
+
         self.dgamma = dgamma
         self.dbeta = dbeta
-        
+
         return dx
 
 
@@ -201,12 +202,12 @@ class Convolution:
         self.b = b
         self.stride = stride
         self.pad = pad
-        
+
         # 중간 데이터（backward 시 사용）
-        self.x = None   
+        self.x = None
         self.col = None
         self.col_W = None
-        
+
         # 가중치와 편향 매개변수의 기울기
         self.dW = None
         self.db = None
@@ -249,7 +250,7 @@ class Pooling:
         self.pool_w = pool_w
         self.stride = stride
         self.pad = pad
-        
+
         self.x = None
         self.arg_max = None
 
@@ -272,13 +273,13 @@ class Pooling:
 
     def backward(self, dout):
         dout = dout.transpose(0, 2, 3, 1)
-        
+
         pool_size = self.pool_h * self.pool_w
         dmax = np.zeros((dout.size, pool_size))
         dmax[np.arange(self.arg_max.size), self.arg_max.flatten()] = dout.flatten()
-        dmax = dmax.reshape(dout.shape + (pool_size,)) 
-        
+        dmax = dmax.reshape(dout.shape + (pool_size,))
+
         dcol = dmax.reshape(dmax.shape[0] * dmax.shape[1] * dmax.shape[2], -1)
         dx = col2im(dcol, self.x.shape, self.pool_h, self.pool_w, self.stride, self.pad)
-        
+
         return dx
